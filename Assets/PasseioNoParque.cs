57ab4a6cdcfcb1cf30fda4cf6d85d0c1451adb25
João Paulo <joaopaulo761@gmail.com>
2018-06-28 11:45:43 -0300

    Inicial

---
+++ Assets/PasseioNoParque.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasseioNoParque : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal, 0, vertical) * 5 * Time.deltaTime);
+	}
+}
